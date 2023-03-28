3f1c700d5f0abea7241d3714f2dcf8815438ff68
d111v <129206329+d111v@users.noreply.github.com>
2023-03-28 20:50:11 +0300

    Add files via upload

---
+++ algopapka/pingpong.py
@@ -0,0 +1,38 @@
+import time
+import random
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (wight,height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+display.set_caption('Пинг Понг')
+
+background = GameSprite('back.jpg', 0, 0, 0, win_width, win_height)
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+while True:
+    for e in event.get():   
+        if e.type == QUIT: 
+            game = False
+    if finish != True:
+        background.reset()
+    
+    display.update()
+    clock.tick(FPS)
+
+
+
